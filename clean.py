c523770c039011c3abd6c7f7a9d5065842d2813e
lucydunshea <125615453+lucydunshea@users.noreply.github.com>
2023-06-07 11:19:30 +1200

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,12 +1,13 @@
 import re   # regular expession
 
-class cleaner:  # for cleaning emails ready for ML
+class cleaner:  # for cleaning emails ready for NB
 
     def __init__(self, subject_file):
         self.subject = open(subject_file, 'rt')
 
-    def splitter(self):
-
+    def splitter(self): # Split text file into string of uncleaned words
+        for line in self.subject:
+            
 
     def drop_emoji(self):
         for line in self.subject:
